742b25900914594203300a847bccde612b889de7
Juliette Woodrow <jwoodrow@stanford.edu>
2025-11-25 18:22:05 -0800

    fixing spacing for photo

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -1,14 +1,20 @@
 
 
-
-export const Face = ({imageUrl="me.jpg", toggleIsVisible=() => {}}) => {
-
-    return <svg width="220" height="220" xmlns="http://www.w3.org/2000/svg" onClick={toggleIsVisible}>
-        <circle cx="110" cy="110" r="100" stroke="pink" strokeWidth="5" fill="none" />
-        <clipPath id="circleView">
-            <circle cx="110" cy="110" r="90" />
-        </clipPath>
-        <image href={imageUrl} width="300" height="300" x="-40" y="-20" clipPath="url(#circleView)" />
+export const Face = ({ imageUrl = "me.jpg", toggleIsVisible = () => {} }) => {
+  return (
+    <svg width="220" height="220" xmlns="http://www.w3.org/2000/svg" onClick={toggleIsVisible}>
+      <circle cx="110" cy="110" r="100" stroke="pink" strokeWidth="5" fill="none" />
+      <clipPath id="circleView">
+        <circle cx="110" cy="110" r="90" />
+      </clipPath>
+      <image
+        href={imageUrl}
+        width="500"
+        height="500"
+        x="-70"
+        y="-100"          // was -20; move image down so the top isn’t clipped
+        clipPath="url(#circleView)"
+      />
     </svg>
-
-}
+  );
+};
