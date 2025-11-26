756d79a6eb43e2ad899980cc3065336bc47566cd
Juliette Woodrow <jwoodrow@stanford.edu>
2025-11-25 18:26:52 -0800

    updating teaching

--- src/components/Teaching.jsx
+++ src/components/Teaching.jsx
@@ -3,6 +3,7 @@ export const Teaching = () => {
         <div className="teaching">
             <h1>Teaching</h1>
             <ul>
+                <li>Lecturer for <a href="https://web.stanford.edu/class/cs109/">CS109: Probability for Computer Scientists</a> (Winter 2026)  </li>
                 <li>Head TA for <a href="https://web.stanford.edu/class/cs109/">CS109: Probability for Computer Scientists</a> (Fall 2025)</li>
                 <li>TA for <a href="https://web.stanford.edu/class/cs109/">CS109: Probability for Computer Scientists</a> (Fall 2023)</li>
                 <li>Lecturer for <a href="https://web.stanford.edu/class/cs106a-8/">CS106A: Programming Methodologies</a> (Summer 2021)</li>
